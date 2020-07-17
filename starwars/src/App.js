433cae5900cbdfefe7463d131ad2e640449a0783
Julliann <jullianndulay@gmail.com>
2020-07-17 09:15:16 -0700

    mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState,useCallback,useEffect } from 'react';
 import './App.css';
+import Character from './components/Character'
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const[characters,setCharacter] = useState([])
+  const[idRandomizer,setIdRandomizer] = useState(0)
+  const rickAndMortyApiBaseUrl = 'https://rickandmortyapi.com/api/character/'
+
+  useEffect(()=>{
+    axios
+    .get(`${rickAndMortyApiBaseUrl}/1,2,3,4`)
+    .then(res=>{        
+      setCharacter(res.data)      
+       
+    })
+    .catch(err=>{
+      console.log('Oh no you have encountered an ',err)
+    })
+  },[])
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(character=>{
+        {return <Character key={character.id} character={character}/>}
+      })}      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+const CharacterStyled = styled.div`
+    width:100%;
+    height: 50vh;
+
+    .flexContainer{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+    }    
+
+
+`
+
+export default function Character (props){
+    const{character} = props;
+
+    return (
+        <CharacterStyled>
+            <div className="flexContainer">
+                <img src={character.image} alt={character.species}/>
+                <div>
+                    <h2>Name: {character.name}</h2>
+                    <h2>Species:{character.species}</h2>
+                    <h2>Gender: {character.gender}</h2>
+                    <h2>Status:{character.status}</h2>
+                </div>
+            </div>            
+        </CharacterStyled>
+    )
+
+}
